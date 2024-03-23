af61817b0e481e1deb1a68240a13177d80b8b436
Alona Didyk <alionadiduk31@gmail.com>
2024-03-23 11:13:22 +0200

    new commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const userName = "Max";
+let age = 23;
+age = 34;
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,50 @@
-// Code goes here!
+const userName = "Max";
+
+let age = 23;
+age = 34;
+
+// function add(a: number, b: number) {
+//   var result;
+//   result = a + b;
+//   return result;
+// }
+
+// if (age > 20) {
+//   var isOld = true;
+// }
+// console.log(isOld);
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput = (output: string | number) => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// const button = document.querySelector("button");
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
+
+// printOutput(add(2));
+
+// const hobbies = ["Sports", "Cooking"];
+// const activeHobbies = ["Hiking"];
+// activeHobbies.push(...hobbies);
+
+// const person = {
+//   firstName: "Max",
+//   age: 30,
+// };
+
+// const copiedPerson = { ...person };
+
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((acc, curr) => acc + curr, 0);
+// };
+
+// const addNumbers = add(2, 4, 5, 3);
+// console.log(addNumbers);
+
+// const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+// const { firstName: userName, age } = person;
+// console.log(userName, age);
